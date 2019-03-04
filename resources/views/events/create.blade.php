07d586d23409ef3cbad22567fd0c2e35e9d3ae9d
Rubenwest <rswester@avans.nl>
2019-03-04 13:21:23 +0100

    Kleine verandering

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <script>
-                    window.onload = function() { 
+    window.onload = function() { 
                         var textarea = document.getElementById('desc');
                         var text = document.getElementById('title');
                         var len_d = parseInt(textarea.getAttribute("maxlength"), 10); 
@@ -46,7 +46,7 @@
                         <img class="selected" src="https://www.yadu.nu/assets/img/icons/categories/white/food-outline_cocktail.svg">
                         <span>Borrelen</span>
                     </label>
-                    
+
                     <!--
                     <div class="category" title="Koffie">
                         <img class="default" src="https://www.yadu.nu/assets/img/icons/categories/food-outline_coffee.svg">
@@ -105,7 +105,7 @@
         </div>
         <div class="date">
             <h3>4. Kies de datum en tijd</h3>
-
+            
         </div>
         <div>
             <h3>5. Beschrijf je uitje</h3>
@@ -114,15 +114,15 @@
                 <span id="chars_title"></span> characters remaining
                 <textarea id="desc" name="description" placeholder="Omschrijving.." oninput="update_counter_desc(this)" maxlength="150" required></textarea>
                 <span id="chars_desc"></span> characters remaining
-               
+
             </div>
         </div>
         <div>
             <!-- Met inspect element krijg ik het nog voor elkaar om meer dan 25 in te stellen, dit moet ik nog veranderen door js scriptje ofzo toe te voegen! -->
             <h3>6. Hoeveel mensen gaan er max mee?</h3>
             <div class="description">
-            <input type="number" name="people" min="1" max="25" value="1" required >
-            <span class="number_desc">mensen kunnen mee (incl. jezelf)</span>
+                <input type="number" name="people" min="1" max="25" value="1" required>
+                <span class="number_desc">mensen kunnen mee (incl. jezelf)</span>
             </div>
         </div>
         <input class="submit" type="submit" name="verzenden" value="Verzend!!">

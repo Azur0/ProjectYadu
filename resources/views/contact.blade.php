1536f2d4ced8fda9b2b70f0894ae53a16f39380a
azur0 <bimre@avans.nl>
2019-03-06 13:23:18 +0100

    no message

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -16,70 +16,47 @@
 
 @section('content')
     <div class="row">
-        <div class="col-sm">
-            <div class="card center-block text-center" style="width: 18rem;">
-                <div class="card-body">
-                    <i class="fa fa-envelope" style="font-size: 48px; color: #E79535"></i>
-                    <h5 class="card-title">E-mail</h5>
-                    <h6 class="card-subtitle mb-2 text-muted"><a href="mailto:info@YADU.nu">info@YADU.nu</a></h6>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm">
-            <div class="card center-block text-center" style="width: 18rem;">
-                <div class="card-body">
-                    <i class="fa fa-home" style="font-size: 48px; color: #E79535"></i>
-                    <h5 class="card-title">Website</h5>
-                    <h6 class="card-subtitle mb-2 text-muted"><a href="http://www.yadu.nu">yadu.nu</a></h6>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm">
-            <div class="card center-block text-center" style="width: 18rem;">
-                <div class="card-body">
-                    <i class="fa fa-building" style="font-size: 48px; color: #E79535"></i>
-                    <h5 class="card-title">KvK</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">62039482</h6>
-                </div>
-            </div>
+        <div class="col-sm text-center">
+            <h2>
+                <b>Lorem ipsum</b>
+            </h2>
         </div>
     </div>
-    <div class="row">
-        <div class="col-sm">
-            <div class="card center-block text-center" style="width: 18rem;">
-                <div class="card-body">
-                    <i class="fa fa-linkedin" style="font-size: 48px; color: #E79535"></i>
-                    <h5 class="card-title">LinkedIn</h5>
-                    <h6 class="card-subtitle mb-2 text-muted"><a href="https://nl.linkedin.com/in/carmentoelen">Carmen Toelen</a></h6>
-                </div>
-            </div>
+    <div class="row about-text">
+        <div class="col-sm text-left">
+            Nam sit amet tempor sem, sit amet volutpat libero. Proin nulla ante, congue vitae semper eget, vestibulum ac est. Morbi ac est varius, pretium libero non, vulputate leo. Praesent efficitur arcu neque, ut maximus metus facilisis vel. Nulla ut egestas nunc. Vivamus cursus urna non sem maximus pharetra. Cras eget placerat mauris. Quisque arcu libero, rhoncus in massa viverra, sollicitudin imperdiet nisl. Fusce non libero vel risus consequat convallis a nec nisi. Vivamus interdum nisi non est tempor, nec maximus tortor fermentum. Nullam sed odio euismod dolor rhoncus efficitur. Vestibulum iaculis laoreet sollicitudin. Mauris magna libero, tempus vitae mollis quis, fermentum non mauris. In quis tellus massa. Vestibulum hendrerit est ante, in fermentum mauris scelerisque non.
         </div>
-        <div class="col-sm">
-            <div class="card center-block text-center" style="width: 18rem;">
-                <div class="card-body">
-                    <i class="fa fa-twitter" style="font-size: 48px; color: #E79535"></i>
-                    <h5 class="card-title">Twitter</h5>
-                    <h6 class="card-subtitle mb-2 text-muted"><a href="https://twitter.com/_Yadu_">@_Yadu_</a></h6>
-                </div>
+        <div class="col-sm text-left">
+            <div class="contact-item">
+                <h5>EMAIL</h5>
+                <a href="mailto:info@YADU.nu" class="contact-item">
+                    <span>info@YADU.nu</span>
+                </a>
             </div>
-        </div>
-        <div class="col-sm">
-            <div class="card center-block text-center" style="width: 18rem;">
-                <div class="card-body">
-                    <i class="fa fa-facebook" style="font-size: 48px; color: #E79535"></i>
-                    <h5 class="card-title">Facebook</h5>
-                    <h6 class="card-subtitle mb-2 text-muted"><a href="https://www.facebook.com/yadu.nu/?ref=bookmarks">@yadu.nu</a></h6>
-                </div>
+            <div class="contact-item">
+                <h5>KvK</h5>
+                <span>62039482</span>
             </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-sm">
-            <div class="card center-block text-center" style="width: 18rem;">
-                <div class="card-body">
-                    <i class="fa fa-instagram" style="font-size: 48px; color: #E79535"></i>
-                    <h5 class="card-title">Instagram</h5>
-                    <h6 class="card-subtitle mb-2 text-muted"><a href="https://www.instagram.com/yadu.nu/">yadu.nu</a></h6>
+            <div class="row">
+                <div class="col-xs-6 media linkedin">
+                    <a href="https://nl.linkedin.com/in/carmentoelen" class="contact-item">
+                        <i class="fa fa-linkedin" style="font-size: 36px; color: #E79535"></i>
+                    </a>
+                </div>
+                <div class="col-xs-6 media twitter">
+                    <a href="https://twitter.com/_Yadu_" class="contact-item">
+                        <i class="fa fa-twitter" style="font-size: 36px; color: #E79535"></i>
+                    </a>
+                </div>
+                <div class="col-xs-6 media facebook">
+                    <a href="https://www.facebook.com/yadu.nu/?ref=bookmarks" class="contact-item">
+                        <i class="fa fa-facebook" style="font-size: 36px; color: #E79535"></i>
+                    </a>
+                </div>
+                <div class="col-xs-6 media instagram">
+                    <a href="https://www.instagram.com/yadu.nu/" class="contact-item">
+                        <i class="fa fa-instagram" style="font-size: 36px; color: #E79535"></i>
+                    </a>
                 </div>
             </div>
         </div>

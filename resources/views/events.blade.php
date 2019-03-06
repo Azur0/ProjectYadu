352eb5f2dde7b53c696779b01d41fa7c2d54db44
SemicolonStruggles <38405582+SemicolonStruggles@users.noreply.github.com>
2019-03-06 13:19:15 +0100

    Temp layout

---
+++ resources/views/events.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts/app')
+
+@section('banner')
+
+@endsection
+
+@section('content')
+    <div class="event_overview">
+
+        <div class="row">
+
+            <div class="col-4 event">
+                <img src={{ asset("images/beer.jpg") }} class="img-responsive" width="100%" alt="Event">
+                <div class="event_info">
+                    Hello
+                </div>
+            </div>
+
+            <div class="col-4 event">
+                <img src={{ asset("images/beer.jpg") }} class="img-responsive" width="100%" alt="Event">
+                <div class="event_info">
+                    Hello
+                </div>
+            </div>
+
+            <div class="col-4 event">
+                <img src={{ asset("images/beer.jpg") }} class="img-responsive" width="100%" alt="Event">
+                <div class="event_info">
+                    Hello
+                </div>
+            </div>
+
+        </div>
+
+        <br>
+
+        <div class="row">
+
+            <div class="col-4 event">
+                <img src={{ asset("images/beer.jpg") }} class="img-responsive" width="100%" alt="Event">
+                <div class="event_info">
+                    Hello
+                </div>
+            </div>
+
+            <div class="col-4 event">
+                <img src={{ asset("images/beer.jpg") }} class="img-responsive" width="100%" alt="Event">
+                <div class="event_info">
+                    Hello
+                </div>
+            </div>
+
+            <div class="col-4 event">
+                <img src={{ asset("images/beer.jpg") }} class="img-responsive" width="100%" alt="Event">
+                <div class="event_info">
+                    Hello
+                </div>
+            </div>
+
+        </div>
+
+        </div>
+
+    </div>
+@endsection

6ed79cd11c4bbd895e6e6525f4da8299ac4f57da
azur0 <bimre@avans.nl>
2019-03-11 10:38:45 +0100

    no message

--- database/migrations/2019_02_25_000020_create_events_table.php
+++ database/migrations/2019_02_25_000020_create_events_table.php
@@ -1,9 +1,7 @@
 <?php
-
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-
 class CreateEventsTable extends Migration
 {
     /**
@@ -11,7 +9,6 @@ class CreateEventsTable extends Migration
      * @var string
      */
     public $tableName = 'events';
-
     /**
      * Run the migrations.
      * @table events
@@ -24,47 +21,51 @@ class CreateEventsTable extends Migration
             $table->engine = 'InnoDB';
             $table->increments('id');
             $table->string('status', 20);
+            $table->string('tag', 25);
             $table->unsignedInteger('location_id');
             $table->unsignedInteger('owner_id');
-            $table->string('activityName', 45);
+            $table->unsignedInteger('event_picture_id');
+            $table->string('eventName', 45);
             $table->dateTime('startDate');
             $table->dateTime('endDate')->nullable();
+            $table->unsignedInteger('numberOfPeople');
             $table->text('description');
-            $table->binary('bannerImage');
             $table->tinyInteger('isDeleted')->default('0');
             $table->timestamps();
-
             $table->index(["location_id"], 'fk_activity_Location1_idx');
-
             $table->index(["status"], 'fk_event_eventStatus1_idx');
-
+            $table->index(["tag"], 'fk_event_eventTags1_idx');
             $table->index(["owner_id"], 'fk_activity_accounts1_idx');
-
-
+            $table->index(["event_picture_id"], 'fk_event_event_pictures_idx');
             $table->foreign('location_id', 'fk_activity_Location1_idx')
                 ->references('id')->on('locations')
                 ->onDelete('no action')
                 ->onUpdate('no action');
-
             $table->foreign('owner_id', 'fk_activity_accounts1_idx')
                 ->references('id')->on('accounts')
                 ->onDelete('no action')
                 ->onUpdate('no action');
-
             $table->foreign('status', 'fk_event_eventStatus1_idx')
-                ->references('status')->on('event_statuses')
+                ->references('status')->on('event_status')
+                ->onDelete('no action')
+                ->onUpdate('no action');
+            $table->foreign('tag', 'fk_event_eventTags1_idx')
+                ->references('tag')->on('event_tags')
+                ->onDelete('no action')
+                ->onUpdate('no action');
+            $table->foreign('event_picture_id', 'fk_event_pictures1_idx')
+                ->references('id')->on('event_pictures')
                 ->onDelete('no action')
                 ->onUpdate('no action');
         });
     }
-
     /**
      * Reverse the migrations.
      *
      * @return void
      */
-     public function down()
-     {
-       Schema::dropIfExists($this->tableName);
-     }
+    public function down()
+    {
+        Schema::dropIfExists($this->tableName);
+    }
 }

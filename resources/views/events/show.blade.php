3c0a03055b7f80871c0793301c632f93270518df
bjamdort <bjamdort@avans.nl>
2019-03-11 15:03:41 +0100

    Added google maps api and added some checks

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-6">
             <div style="background-image: url();">
                 <h1>{{$event->activityName}}</h1><br>
-                <img class="img-fluid" src="https://via.placeholder.com/4000x2000s.jpg"/><br>
+                <?php echo '<img class="img-fluid rounded-circle" width="50" class="my-auto" src="data:image/jpeg;base64,' . base64_encode($event->bannerImage) . '"/>'; ?>
                 {{$event->startDate}} <br><br>
             </div>
 
@@ -32,8 +32,20 @@
                 </div>
             @endforeach
         </div>
-        <div class="col-md-6" style="background: #3f9ae5">
+        <div class="col-md-6">
             {{--TODO: Add map API--}}
+            <div id="map" class="rounded"></div>
+            <script>
+                var map;
+
+                function initMap() {
+                    map = new google.maps.Map(document.getElementById('map'), {
+                        center: {lat: 52.1037242, lng: 5.5593545},
+                        zoom: 7
+                    });
+                }
+            </script>
+            <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAuigrcHjZ0tW0VErNr7_U4Pq_gLCknnD0&callback=initMap" async defer></script>
         </div>
     </div>
 @endsection

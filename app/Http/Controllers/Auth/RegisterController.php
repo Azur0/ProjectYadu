914e07377df0c5d2b8cc579f6175c3c583f259e1
Jeroen3529 <Jeroen3529@gmail.com>
2019-03-11 14:09:14 +0100

    validation lines

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,9 +49,12 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:6', 'confirmed'],
+            'firstName' => ['min:5','max:45','required'],
+            'middleName' => ['min:5','max:45'],
+            'lastName' => ['min:5','max:45'],
+            'dateOfBirth' => 'date',
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'password' => ['required', 'string', 'min:8','confirmed']
         ]);
     }
 
@@ -64,7 +67,10 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            'firstName' => $data['firstName'],
+            'middleName' => $data['middleName'],
+            'lastName' => $data['lastName'],
+            'dateOfBirth' => $data['dateOfBirth'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);

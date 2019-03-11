a6c56cc511c1c23081fcd77d7f971a0e1a907df7
Rubenwest <rswester@avans.nl>
2019-03-11 10:34:55 +0100

    slider ziet er nu mooi uit!

--- resources/views/temp/sliderDemo.blade.php
+++ resources/views/temp/sliderDemo.blade.php
@@ -5,40 +5,36 @@
 @section('nav')
 
 @section('content')
-    <h1>Slider demo page</h1>
-    <!-- This page is to test the slider -->
-    <div class="box-range-value">
-        <div id="rangeValueDisplay">
-        </div>
+<h1>Slider demo page</h1>
+<!-- This page is to test the slider -->
+<div class="box-range-value">
+    <div id="rangeValueDisplay">
     </div>
-    <div class="slideContainer">
-        <input type="range"
-               ticks="[5, 10, 15, 20, 25]"
-               min="5"
-               max="25"
-               step="5"
-               value="15"
-               class="slider" id="rangeValue">
+</div>
+<div class="slideContainer">
+    <input type="range" ticks="[5, 10, 15, 20, 25]" min="5" max="25" step="5" value="15" class="slider" id="rangeValue">
+    <div class="labels">
         <label class="rangeTextLeft">5 KM</label>
         <label class="rangeTextCenter">10 KM</label>
         <label class="rangeTextCenter">15 KM</label>
         <label class="rangeTextCenter">20 KM</label>
         <label class="rangeTextRight">25 KM</label>
     </div>
+</div>
 
 
 
-    <script type="text/javascript">
-        var slider=document.getElementById("rangeValue");
-        var val=document.getElementById("rangeValueDisplay");
-        val.innerHTML=slider.value;
-        slider.oninput=function () {
-            val.innerHTML=this.value;
-        }
-    </script>
+<script type="text/javascript">
+    var slider = document.getElementById("rangeValue");
+    var val = document.getElementById("rangeValueDisplay");
+    val.innerHTML = slider.value;
+    slider.oninput = function() {
+        val.innerHTML = this.value;
+    }
+</script>
 
 @endsection()
 
 
 
-@section('footer')
+@section('footer') 

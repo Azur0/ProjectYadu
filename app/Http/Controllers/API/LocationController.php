f3a8f010029996d185bbc427237ae209d41aadb7
default-kaas <dogen1996@live.nl>
2019-03-13 10:23:13 +0100

    Hard code user location for testing

--- app/Http/Controllers/API/LocationController.php
+++ app/Http/Controllers/API/LocationController.php
@@ -21,7 +21,8 @@ class LocationController extends Controller
     }
 
     private function getLocation(){
-        $ip = self::get_ip();
+        //$ip = self::get_ip();
+        $ip = '145.49.103.72';
         $query = @unserialize(file_get_contents('http://ip-api.com/php/'.$ip));
             if($query && $query['status'] == 'success'){
              return $query;
@@ -49,11 +50,13 @@ class LocationController extends Controller
             return true;
         }
 
-        $userLocation = self::getLocation();
+        //$userLocation = self::getLocation();
         $eventLocation = self::eventLonLat($event);
 
-        if($eventLocation != false && $userLocation != false) {
-            $query3 = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=' . $userLocation['lat'] . ',' . $userLocation['lon'] . '&destinations=' . $eventLocation[0]['lat'] . ',' . $eventLocation[0]['lon'] . '&key=AIzaSyDL4ugHzrWMXq40HaC3KEUtdgoeTVX3JcU';
+        if($eventLocation != false /*&& $userLocation != false*/) {
+            //$query3 = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=' . $userLocation['lat'] . ',' . $userLocation['lon'] . '&destinations=' . $eventLocation[0]['lat'] . ',' . $eventLocation[0]['lon'] . '&key=AIzaSyDL4ugHzrWMXq40HaC3KEUtdgoeTVX3JcU';
+            $query3 = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=51.6886358,5.2870897&destinations=' . $eventLocation[0]['lat'] . ',' . $eventLocation[0]['lon'] . '&key=AIzaSyDL4ugHzrWMXq40HaC3KEUtdgoeTVX3JcU';
+
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, $query3);
             curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13");

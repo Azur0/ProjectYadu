c63341c545178172ba3b57409dd27fcdeebdb100
bjamdort <bjamdort@avans.nl>
2019-04-03 22:51:54 +0200

    Added details

--- app/Account.php
+++ app/Account.php
@@ -10,7 +10,21 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class Account extends Authenticatable
 {
     //
-    protected $fillable = ['firstName', 'middleName', 'lastName', 'dateOfBirth', 'email', 'password','gender'];
+    protected $fillable = ['firstName', 'middleName', 'lastName', 'dateOfBirth', 'email', 'password','gender', 'avatar'];
 
+    public function getAvatarAttribute($key)
+    {
+        $avatar = $this->attributes['avatar'];
 
+//        dd($avatar);
+
+        if ($avatar == null) {
+            $filePath = public_path() . "/images/avatar.png";
+
+            return fread(fopen($filePath, "r"), filesize($filePath));
+        }
+        else {
+            return $avatar;
+        }
+    }
 }
--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -1,18 +1,17 @@
 @extends('layouts/app')
 
 @section('content')
-
     <div class="row">
         <div class="col-md-6">
             <div>
                 <h1>{{$event->eventName}}</h1><br>
-                <img class="img-fluid w-100" src="data:image/jpeg;base64, {{base64_encode($event->eventPicture->picture)}}"/><br>
-                {{$event->startDate}} <br><br>
+                <img class="img-fluid w-100 rounded" src="data:image/jpeg;base64, {{base64_encode($event->eventPicture->picture)}}"/><br>
+                <h4 class="mb-5">{{$event->startDate}}</h4>
             </div>
 
             <h3>Initiatiefnemer</h3>
             <div class="row my-1">
-                <?php echo '<img class="img-fluid rounded-circle" width="50" class="my-auto" src="data:image/jpeg;base64,' . base64_encode($event->owner->avatar) . '"/>'; ?>
+                <img class="img-fluid rounded-circle" width="50" class="my-auto" src="data:image/jpeg;base64, {{base64_encode($event->owner->avatar)}}"/>
                 <h5 class="my-auto ml-2">{{$event->owner->firstName .' '. $event->owner->middleName .' '. $event->owner->lastName}}</h5>
             </div>
             <br><br>
@@ -24,23 +23,32 @@
                         @if($event->participants->contains(auth()->user()->id))
                             <a href="/events/{{$event->id}}/leave"
                                class="btn btn-danger btn-sm my-auto mx-2">Afmelden</a>
-                        @else
+                        @elseif($event->participants->count() < $event->numberOfPeople)
                             <a href="/events/{{$event->id}}/join"
                                class="btn btn-success btn-sm my-auto mx-2">Aanmelden</a>
                         @endif
                     @endif
                 @endif
             </div>
+            <p class="text-md-right">{{$event->participants->count()}} deelnemers van de {{$event->numberOfPeople}}</p>
+            <div class="progress">
+                <div class="progress-bar" role="progressbar" style="width: {{$event->participants->count() / $event->numberOfPeople * 100}}%"  aria-valuemin="0" aria-valuenow="{{$event->participants->count()}}" aria-valuemax="{{$event->numberOfPeople}}"></div>
+            </div>
             @foreach($event->participants as $participant)
                 <div class="row my-1">
-                    <?php echo '<img class="img-fluid rounded-circle" width="50" class="my-auto" src="data:image/jpeg;base64,' . base64_encode($participant->avatar) . '"/>'; ?>
+                    <img class="img-fluid rounded-circle" width="50" class="my-auto" src="data:image/jpeg;base64, {{base64_encode($participant->avatar)}}"/>
                     <h5 class="my-auto ml-2">{{$participant->firstName .' '. $participant->middleName .' '. $participant->lastName}}</h5>
                 </div>
             @endforeach
         </div>
         <div class="col-md-6">
+            <div class="mb-5">
+                <h3>Beschrijving</h3>
+                <p>{{$event->description}}</p>
+            </div>
             {{--TODO: Add map API--}}
-            <div id="map" class="rounded"></div>
+            <h3>Kaart</h3>
+            <div id="map" class="rounded" style="height: 30em;"></div>
             <script>
                 var map;
 
@@ -55,4 +63,5 @@
                     async defer></script>
         </div>
     </div>
+
 @endsection

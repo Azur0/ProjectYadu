08636aff7d3af084da6d9aba99ffe09f9d69b194
default-kaas <dogen1996@live.nl>
2019-04-03 21:40:11 +0200

    Location filter done
    Co-Authored-By: Rubenwest <rswester@users.noreply.github.com>

--- app/Http/Controllers/API/LocationController.php
+++ app/Http/Controllers/API/LocationController.php
@@ -6,6 +6,7 @@ use App\Event;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Services\IpApi;
+use Illuminate\Database\Eloquent\Collection;
 
 class LocationController extends Controller
 {
@@ -21,8 +22,8 @@ class LocationController extends Controller
     }
 
     private function getLocation(){
-        //$ip = self::get_ip();
-        $ip = '145.49.103.72';
+        $ip = self::get_ip();
+        //$ip = '145.49.103.72';
         $query = @unserialize(file_get_contents('http://ip-api.com/php/'.$ip));
             if($query && $query['status'] == 'success'){
              return $query;
@@ -31,95 +32,45 @@ class LocationController extends Controller
             }
     }
 
-    private function eventLonLat(Event $event){
+    public function areWithinReach($events, $distance)
+    {
+        if ($distance == 25) {
+            return $events;
+        }
+        $userLocation = self::getLocation();
+        $front = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=52.161436,5.416094&destinations=';
+        //$front = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=';
+        $userLocation = $userLocation['lat'] . ',' . $userLocation['lon'];
+        $destination = '&destinations=';
+        $EndapiKey = '&key=AIzaSyDL4ugHzrWMXq40HaC3KEUtdgoeTVX3JcU';
+        $eventsToReturn = new Collection();
+        for ($i = 0; $i <= ceil(count($events) / 25); $i++) {
+            $slicedArray = $events->splice(25 * $i, 25);
+            $locations = "";
+            foreach ($slicedArray as $slice) {
+                $locations .= $slice->location->locLatitude . '%2C' . $slice->location->locLongtitude . '%7C';
+            }
+            $locations = substr($locations, 0, -3);
+            //$request = $front . $userLocation . $destination .$locations . $EndapiKey;
+            $request = $front . $locations . $EndapiKey;
+            $eventDistances = $this->googleRequest($request);
+            for ($j = 0; $j < count($slicedArray); $j++) {
+                if ($eventDistances['rows'][0]['elements'][$j]['distance']['value'] <= ($distance * 1000)) {
+                    $eventsToReturn->push($slicedArray[$j]);
+                }
+            }
+        }
+        return $eventsToReturn;
+    }
 
-        $eventZipCode = $event->location->postalcode;
-        $query2 = "https://nominatim.openstreetmap.org/search?q=".$eventZipCode."&format=json&addressdetails=1";
+    public function googleRequest($request){
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $query2);
+        curl_setopt($ch, CURLOPT_URL, $request);
         curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $result = curl_exec($ch);
         curl_close($ch);
         $json = json_decode($result, true);
         return $json;
-    }
-
-    public function isWithinReach(Event $event, $distance){
-        if($distance == 25){
-            return true;
-        }
-
-        //$userLocation = self::getLocation();
-        $eventLocation = self::eventLonLat($event);
-
-        if($eventLocation != false /*&& $userLocation != false*/) {
-            //$query3 = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=' . $userLocation['lat'] . ',' . $userLocation['lon'] . '&destinations=' . $eventLocation[0]['lat'] . ',' . $eventLocation[0]['lon'] . '&key=AIzaSyDL4ugHzrWMXq40HaC3KEUtdgoeTVX3JcU';
-            $query3 = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=51.6886358,5.2870897&destinations=' . $eventLocation[0]['lat'] . ',' . $eventLocation[0]['lon'] . '&key=AIzaSyDL4ugHzrWMXq40HaC3KEUtdgoeTVX3JcU';
-
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $query3);
-            curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13");
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            $result = curl_exec($ch);
-            curl_close($ch);
-            $json = json_decode($result, true);
-            if($json['rows'][0]['elements'][0]['distance']['value'] <= ($distance*1000)){
-            return true;
-            }else{
-            return false;
-            }
-        }
-        return false;
-    }
-
-    public function areWithinReach($events, $distance){
-        if($distance == 25){
-            return true;
-        }
-
-        //$userLocation = self::getLocation();
-        //TODO set foreach event lat and lon
-        $eventLocation = self::eventLonLat($event);
-
-        //
-        //TODO remove all events that the location can't be found of
-        //TODO foreach^
-        if($eventLocation != false /*&& $userLocation != false*/) {
-
-
-            //TODO make part set for events
-                //TODO remove from list
-                //TODO add to new collect of events to be controlled
-            //
-            //TODO make string with all the lons and lats of events
-
-            //TODO add it to the Query3
-
-            //TODO add api key to the end of the query
-            //$query3 = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=' . $userLocation['lat'] . ',' . $userLocation['lon'] . '&destinations=' . $eventLocation[0]['lat'] . ',' . $eventLocation[0]['lon'] . '&key=AIzaSyDL4ugHzrWMXq40HaC3KEUtdgoeTVX3JcU';
-            $query3 = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=51.6886358,5.2870897&destinations=' . $eventLocation[0]['lat'] . ',' . $eventLocation[0]['lon'] . '&key=AIzaSyDL4ugHzrWMXq40HaC3KEUtdgoeTVX3JcU';
-
-            //TODO send request
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $query3);
-            curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13");
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            $result = curl_exec($ch);
-            curl_close($ch);
-            $json = json_decode($result, true);
-
-            //TODO keep events in distance (based on array place)
-            //TODO make foreach^
-                if($json['rows'][0]['elements'][0]['distance']['value'] <= ($distance*1000)){
-                    //TODO add to list to return of events to show
-                    return true;
-                }else{
-                    //TODO remove this
-                    return false;
-                }
-        }
-        //TODO return list of events to show
-        return false;
     }
 }

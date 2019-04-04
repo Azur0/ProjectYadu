46ad96b2fa2113b2b0a0dce67c5b8c2e613a9492
SemicolonStruggles <38405582+SemicolonStruggles@users.noreply.github.com>
2019-04-04 17:46:39 +0200

    Login errors display correctly
    - Firstname error for incorrect input now displays correcly.
    - Changed firstname min-length to 1 character

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -56,7 +56,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'firstName' => ['required','min:5','max:45','string'],
+            'firstName' => ['required','min:1','max:45','string'],
             'middleName' => ['nullable', 'max:45','string'],
             'lastName' => ['nullable', 'max:45','string'],
             'dateOfBirth' => ['nullable', 'date'],

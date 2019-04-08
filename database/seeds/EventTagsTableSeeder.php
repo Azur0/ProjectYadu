31d72cb82f8f06bd857615c1f871051c5f88979f
SemicolonStruggles <38405582+SemicolonStruggles@users.noreply.github.com>
2019-04-08 03:04:50 +0200

    Added event_tags (no images)

--- database/seeds/EventTagsTableSeeder.php
+++ database/seeds/EventTagsTableSeeder.php
@@ -11,6 +11,79 @@ class EventTagsTableSeeder extends Seeder
      */
     public function run()
     {
-        factory('App\EventTag')->create();
+        //factory('App\EventTag')->create();
+
+        DB::table('event_tags')->insert([
+            'tag' => 'Uitje met gezinnen',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+        ]);
+
+        DB::table('event_tags')->insert([
+            'tag' => 'Eten',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+        ]);
+
+        DB::table('event_tags')->insert([
+            'tag' => 'Borrelen',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+        ]);
+
+        DB::table('event_tags')->insert([
+            'tag' => 'Koffie',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+        ]);
+
+        DB::table('event_tags')->insert([
+            'tag' => 'Wandelen',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+        ]);
+
+        DB::table('event_tags')->insert([
+            'tag' => 'Uitje in stad',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+        ]);
+
+        DB::table('event_tags')->insert([
+            'tag' => 'Museum',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+        ]);
+
+        DB::table('event_tags')->insert([
+            'tag' => 'Theater/film',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+        ]);
+
+        DB::table('event_tags')->insert([
+            'tag' => 'Evenementen',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+        ]);
+
+        DB::table('event_tags')->insert([
+            'tag' => 'Van alles wat',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+        ]);
+
+        DB::table('event_tags')->insert([
+            'tag' => 'Sporten',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+        ]);
+
+        DB::table('event_tags')->insert([
+            'tag' => 'Op wielen',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+        ]);
+
     }
 }

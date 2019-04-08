8e5f7488dc801a015060ff759976ce3516e7f6c8
SemicolonStruggles <38405582+SemicolonStruggles@users.noreply.github.com>
2019-04-08 13:21:44 +0200

    Now seeding eventtags images

--- database/seeds/EventTagsTableSeeder.php
+++ database/seeds/EventTagsTableSeeder.php
@@ -11,15 +11,7 @@ class EventTagsTableSeeder extends Seeder
      */
     public function run()
     {
-        //TODO images
         //factory('App\EventTag')->create();
-
-        $filePath = public_path() . "/images/avatar.png";
-
-        //return fread(fopen($filePath, "r"), filesize($filePath));
-
-        //$filePath = public_path() . "/images/avatar.png";
-
 
         $filePathDefault = public_path() . "/images/Event_Tags/uitje_met_gezinnen.png";
         $filePathSelected = public_path() . "/images/Event_Tags/uitje_met_gezinnen_1.png";
@@ -29,74 +21,128 @@ class EventTagsTableSeeder extends Seeder
             'created_at' => date("Y-m-d H:i:s"),
             'updated_at' => date("Y-m-d H:i:s"),
             'imageDefault' => fread(fopen($filePathDefault , "r"), filesize($filePathDefault)),
-            'imageSelected' => fread(fopen($filePathDefault , "r"), filesize($filePathDefault))
+            'imageSelected' => fread(fopen($filePathSelected , "r"), filesize($filePathSelected))
         ]);
+
+        $filePathDefault = public_path() . "/images/Event_Tags/eten.png";
+        $filePathSelected = public_path() . "/images/Event_Tags/eten_1.png";
 
         DB::table('event_tags')->insert([
             'tag' => 'Eten',
             'created_at' => date("Y-m-d H:i:s"),
             'updated_at' => date("Y-m-d H:i:s"),
+            'imageDefault' => fread(fopen($filePathDefault , "r"), filesize($filePathDefault)),
+            'imageSelected' => fread(fopen($filePathSelected , "r"), filesize($filePathSelected))
         ]);
+
+        $filePathDefault = public_path() . "/images/Event_Tags/borrelen.png";
+        $filePathSelected = public_path() . "/images/Event_Tags/borrelen_1.png";
 
         DB::table('event_tags')->insert([
             'tag' => 'Borrelen',
             'created_at' => date("Y-m-d H:i:s"),
             'updated_at' => date("Y-m-d H:i:s"),
+            'imageDefault' => fread(fopen($filePathDefault , "r"), filesize($filePathDefault)),
+            'imageSelected' => fread(fopen($filePathSelected , "r"), filesize($filePathSelected))
         ]);
+
+        $filePathDefault = public_path() . "/images/Event_Tags/koffie.png";
+        $filePathSelected = public_path() . "/images/Event_Tags/koffie_1.png";
 
         DB::table('event_tags')->insert([
             'tag' => 'Koffie',
             'created_at' => date("Y-m-d H:i:s"),
             'updated_at' => date("Y-m-d H:i:s"),
+            'imageDefault' => fread(fopen($filePathDefault , "r"), filesize($filePathDefault)),
+            'imageSelected' => fread(fopen($filePathSelected , "r"), filesize($filePathSelected))
         ]);
+
+        $filePathDefault = public_path() . "/images/Event_Tags/wandelen.png";
+        $filePathSelected = public_path() . "/images/Event_Tags/wandelen_1.png";
 
         DB::table('event_tags')->insert([
             'tag' => 'Wandelen',
             'created_at' => date("Y-m-d H:i:s"),
             'updated_at' => date("Y-m-d H:i:s"),
+            'imageDefault' => fread(fopen($filePathDefault , "r"), filesize($filePathDefault)),
+            'imageSelected' => fread(fopen($filePathSelected , "r"), filesize($filePathSelected))
         ]);
+
+        $filePathDefault = public_path() . "/images/Event_Tags/uitje_in_stad.png";
+        $filePathSelected = public_path() . "/images/Event_Tags/uitje_in_stad_1.png";
 
         DB::table('event_tags')->insert([
             'tag' => 'Uitje in stad',
             'created_at' => date("Y-m-d H:i:s"),
             'updated_at' => date("Y-m-d H:i:s"),
+            'imageDefault' => fread(fopen($filePathDefault , "r"), filesize($filePathDefault)),
+            'imageSelected' => fread(fopen($filePathSelected , "r"), filesize($filePathSelected))
         ]);
+
+        $filePathDefault = public_path() . "/images/Event_Tags/museum.png";
+        $filePathSelected = public_path() . "/images/Event_Tags/museum_1.png";
 
         DB::table('event_tags')->insert([
             'tag' => 'Museum',
             'created_at' => date("Y-m-d H:i:s"),
             'updated_at' => date("Y-m-d H:i:s"),
+            'imageDefault' => fread(fopen($filePathDefault , "r"), filesize($filePathDefault)),
+            'imageSelected' => fread(fopen($filePathSelected , "r"), filesize($filePathSelected))
         ]);
+
+        $filePathDefault = public_path() . "/images/Event_Tags/theater_film.png";
+        $filePathSelected = public_path() . "/images/Event_Tags/theater_film_1.png";
 
         DB::table('event_tags')->insert([
             'tag' => 'Theater/film',
             'created_at' => date("Y-m-d H:i:s"),
             'updated_at' => date("Y-m-d H:i:s"),
+            'imageDefault' => fread(fopen($filePathDefault , "r"), filesize($filePathDefault)),
+            'imageSelected' => fread(fopen($filePathSelected , "r"), filesize($filePathSelected))
         ]);
+
+        $filePathDefault = public_path() . "/images/Event_Tags/evenementen.png";
+        $filePathSelected = public_path() . "/images/Event_Tags/evenementen_1.png";
 
         DB::table('event_tags')->insert([
             'tag' => 'Evenementen',
             'created_at' => date("Y-m-d H:i:s"),
             'updated_at' => date("Y-m-d H:i:s"),
+            'imageDefault' => fread(fopen($filePathDefault , "r"), filesize($filePathDefault)),
+            'imageSelected' => fread(fopen($filePathSelected , "r"), filesize($filePathSelected))
         ]);
+
+        $filePathDefault = public_path() . "/images/Event_Tags/alles_wat.png";
+        $filePathSelected = public_path() . "/images/Event_Tags/alles_wat_1.png";
 
         DB::table('event_tags')->insert([
             'tag' => 'Van alles wat',
             'created_at' => date("Y-m-d H:i:s"),
             'updated_at' => date("Y-m-d H:i:s"),
+            'imageDefault' => fread(fopen($filePathDefault , "r"), filesize($filePathDefault)),
+            'imageSelected' => fread(fopen($filePathSelected , "r"), filesize($filePathSelected))
         ]);
+
+        $filePathDefault = public_path() . "/images/Event_Tags/sporten.png";
+        $filePathSelected = public_path() . "/images/Event_Tags/sporten_1.png";
 
         DB::table('event_tags')->insert([
             'tag' => 'Sporten',
             'created_at' => date("Y-m-d H:i:s"),
             'updated_at' => date("Y-m-d H:i:s"),
+            'imageDefault' => fread(fopen($filePathDefault , "r"), filesize($filePathDefault)),
+            'imageSelected' => fread(fopen($filePathSelected , "r"), filesize($filePathSelected))
         ]);
+
+        $filePathDefault = public_path() . "/images/Event_Tags/op_wielen.png";
+        $filePathSelected = public_path() . "/images/Event_Tags/op_wielen_1.png";
 
         DB::table('event_tags')->insert([
             'tag' => 'Op wielen',
             'created_at' => date("Y-m-d H:i:s"),
             'updated_at' => date("Y-m-d H:i:s"),
+            'imageDefault' => fread(fopen($filePathDefault , "r"), filesize($filePathDefault)),
+            'imageSelected' => fread(fopen($filePathSelected , "r"), filesize($filePathSelected))
         ]);
-
     }
 }

fbf87586f901443b3e1626d72de59850d5005c08
SemicolonStruggles <38405582+SemicolonStruggles@users.noreply.github.com>
2019-04-08 21:10:40 +0200

    Added correct images to event

--- database/seeds/EventsTableSeeder.php
+++ database/seeds/EventsTableSeeder.php
@@ -12,7 +12,6 @@ class EventsTableSeeder extends Seeder
     public function run()
     {
         //TODO descriptions
-        //TODO images
 
         //factory('App\Event', 10)->create();
         $date = date("Y-m-d H:i:s");
@@ -22,7 +21,7 @@ class EventsTableSeeder extends Seeder
             'tag_id' => 1,
             'location_id' => 1,
             'owner_id' => 1,
-            'event_picture_id' => 1,
+            'event_picture_id' => 14,
             'eventName' => 'Met zijn alle naar het zwembad',
             'startDate' => date('Y-m-d', strtotime($date. ' + 10 days')),
             'endDate' => date('Y-m-d', strtotime($date. ' + 11 days')),
@@ -38,7 +37,7 @@ class EventsTableSeeder extends Seeder
             'tag_id' => 2,
             'location_id' => 2,
             'owner_id' => 2,
-            'event_picture_id' => 2,
+            'event_picture_id' => 27,
             'eventName' => 'Onbeperkt tapas eten!',
             'startDate' => date('Y-m-d', strtotime($date. ' + 10 days')),
             'endDate' => date('Y-m-d', strtotime($date. ' + 11 days')),
@@ -54,7 +53,7 @@ class EventsTableSeeder extends Seeder
             'tag_id' => 3,
             'location_id' => 3,
             'owner_id' => 3,
-            'event_picture_id' => 3,
+            'event_picture_id' => 33,
             'eventName' => 'Zaterdag biertje doen?',
             'startDate' => date('Y-m-d', strtotime($date. ' + 10 days')),
             'endDate' => date('Y-m-d', strtotime($date. ' + 11 days')),
@@ -70,7 +69,7 @@ class EventsTableSeeder extends Seeder
             'tag_id' => 4,
             'location_id' => 4,
             'owner_id' => 4,
-            'event_picture_id' => 4,
+            'event_picture_id' => 52,
             'eventName' => 'Koffie in het buurthuis',
             'startDate' => date('Y-m-d', strtotime($date. ' + 10 days')),
             'endDate' => date('Y-m-d', strtotime($date. ' + 11 days')),
@@ -86,7 +85,7 @@ class EventsTableSeeder extends Seeder
             'tag_id' => 5,
             'location_id' => 5,
             'owner_id' => 5,
-            'event_picture_id' => 5,
+            'event_picture_id' => 59,
             'eventName' => 'Wandelen in het bos',
             'startDate' => date('Y-m-d', strtotime($date. ' + 10 days')),
             'endDate' => date('Y-m-d', strtotime($date. ' + 11 days')),
@@ -102,7 +101,7 @@ class EventsTableSeeder extends Seeder
             'tag_id' => 6,
             'location_id' => 6,
             'owner_id' => 6,
-            'event_picture_id' => 6,
+            'event_picture_id' => 80,
             'eventName' => 'Gezellig samen koopjes jagen',
             'startDate' => date('Y-m-d', strtotime($date. ' + 10 days')),
             'endDate' => date('Y-m-d', strtotime($date. ' + 11 days')),
@@ -118,7 +117,7 @@ class EventsTableSeeder extends Seeder
             'tag_id' => 7,
             'location_id' => 7,
             'owner_id' => 7,
-            'event_picture_id' => 7,
+            'event_picture_id' => 90,
             'eventName' => 'Naar het oorlogsmuseum',
             'startDate' => date('Y-m-d', strtotime($date. ' + 10 days')),
             'endDate' => date('Y-m-d', strtotime($date. ' + 11 days')),
@@ -134,7 +133,7 @@ class EventsTableSeeder extends Seeder
             'tag_id' => 8,
             'location_id' => 8,
             'owner_id' => 8,
-            'event_picture_id' => 8,
+            'event_picture_id' => 106,
             'eventName' => 'Film marthon met popcorn!',
             'startDate' => date('Y-m-d', strtotime($date. ' + 10 days')),
             'endDate' => date('Y-m-d', strtotime($date. ' + 11 days')),
@@ -150,7 +149,7 @@ class EventsTableSeeder extends Seeder
             'tag_id' => 9,
             'location_id' => 9,
             'owner_id' => 9,
-            'event_picture_id' => 9,
+            'event_picture_id' => 136,
             'eventName' => 'Tweakers security meetup',
             'startDate' => date('Y-m-d', strtotime($date. ' + 10 days')),
             'endDate' => date('Y-m-d', strtotime($date. ' + 11 days')),
@@ -166,7 +165,7 @@ class EventsTableSeeder extends Seeder
             'tag_id' => 10,
             'location_id' => 10,
             'owner_id' => 10,
-            'event_picture_id' => 10,
+            'event_picture_id' => 145,
             'eventName' => 'Nieuwe drone testen in het park',
             'startDate' => date('Y-m-d', strtotime($date. ' + 10 days')),
             'endDate' => date('Y-m-d', strtotime($date. ' + 11 days')),
@@ -182,8 +181,8 @@ class EventsTableSeeder extends Seeder
             'tag_id' => 11,
             'location_id' => 11,
             'owner_id' => 1,
-            'event_picture_id' => 11,
-            'eventName' => 'Nieuwe drone testen in het park',
+            'event_picture_id' => 163,
+            'eventName' => 'Tafeltennis avond',
             'startDate' => date('Y-m-d', strtotime($date. ' + 10 days')),
             'endDate' => date('Y-m-d', strtotime($date. ' + 11 days')),
             'numberOfPeople' => 5,
@@ -198,8 +197,8 @@ class EventsTableSeeder extends Seeder
             'tag_id' => 12,
             'location_id' => 12,
             'owner_id' => 1,
-            'event_picture_id' => 12,
-            'eventName' => 'Nieuwe drone testen in het park',
+            'event_picture_id' => 185,
+            'eventName' => 'Skateboarden in het park',
             'startDate' => date('Y-m-d', strtotime($date. ' + 10 days')),
             'endDate' => date('Y-m-d', strtotime($date. ' + 11 days')),
             'numberOfPeople' => 5,
@@ -208,6 +207,5 @@ class EventsTableSeeder extends Seeder
             'created_at' => date("Y-m-d H:i:s"),
             'updated_at' => date("Y-m-d H:i:s")
         ]);
-
     }
 }

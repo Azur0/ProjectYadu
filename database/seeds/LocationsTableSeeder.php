8f4deeddf75516cbcc7476ca51f8f1117021c31b
SemicolonStruggles <38405582+SemicolonStruggles@users.noreply.github.com>
2019-04-08 02:18:10 +0200

    Added locations

--- database/seeds/LocationsTableSeeder.php
+++ database/seeds/LocationsTableSeeder.php
@@ -11,6 +11,86 @@ class LocationsTableSeeder extends Seeder
      */
     public function run()
     {
-        factory('App\Location', 10)->create();
+        //factory('App\Location', 10)->create();
+
+        DB::table('locations')->insert([
+            'houseNumber' => '100',
+            'houseNumberAddition' => 'A',
+            'postalCode' => '5211GL',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('locations')->insert([
+            'houseNumber' => '115',
+            'houseNumberAddition' => null,
+            'postalCode' => '5223HH',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('locations')->insert([
+            'houseNumber' => '150',
+            'houseNumberAddition' => null,
+            'postalCode' => '5237DE',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('locations')->insert([
+            'houseNumber' => '5',
+            'houseNumberAddition' => null,
+            'postalCode' => '5301BE',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('locations')->insert([
+            'houseNumber' => '1',
+            'houseNumberAddition' => null,
+            'postalCode' => '5342AJ',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('locations')->insert([
+            'houseNumber' => '10',
+            'houseNumberAddition' => null,
+            'postalCode' => '6525GA',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('locations')->insert([
+            'houseNumber' => '142',
+            'houseNumberAddition' => null,
+            'postalCode' => '5481AJ',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('locations')->insert([
+            'houseNumber' => '5',
+            'houseNumberAddition' => null,
+            'postalCode' => '5282HK',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('locations')->insert([
+            'houseNumber' => '9',
+            'houseNumberAddition' => null,
+            'postalCode' => '5268BZ',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('locations')->insert([
+            'houseNumber' => '1',
+            'houseNumberAddition' => null,
+            'postalCode' => '9711ME',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
     }
 }

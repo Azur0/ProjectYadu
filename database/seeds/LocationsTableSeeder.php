99a17d778719772c167c928e004670b8d1a65fac
SemicolonStruggles <38405582+SemicolonStruggles@users.noreply.github.com>
2019-04-09 18:18:54 +0200

    Optimized location seeder

--- database/seeds/LocationsTableSeeder.php
+++ database/seeds/LocationsTableSeeder.php
@@ -13,100 +13,19 @@ class LocationsTableSeeder extends Seeder
     {
         //factory('App\Location', 10)->create();
 
-        DB::table('locations')->insert([
-            'houseNumber' => '100',
-            'houseNumberAddition' => 'A',
-            'postalCode' => '5211GL',
-            'created_at' => date("Y-m-d H:i:s"),
-            'updated_at' => date("Y-m-d H:i:s")
-        ]);
+        //Arrays need to be the same size!
+        $numbers = array(100, 115, 150, 5, 1, 10, 142, 5, 9, 1, 153, 2);
+        $additions = array("A", null, null, null, null, null, null, null, null, null, null, null);
+        $codes = array("5211GL", "5223HH", "5237DE", "5301BE", "5342AJ", "6525GA", "5481AJ", "5282HK", "5268BZ", "9711ME", "2911RE", "3439MX");
 
-        DB::table('locations')->insert([
-            'houseNumber' => '115',
-            'houseNumberAddition' => null,
-            'postalCode' => '5223HH',
-            'created_at' => date("Y-m-d H:i:s"),
-            'updated_at' => date("Y-m-d H:i:s")
-        ]);
-
-        DB::table('locations')->insert([
-            'houseNumber' => '150',
-            'houseNumberAddition' => null,
-            'postalCode' => '5237DE',
-            'created_at' => date("Y-m-d H:i:s"),
-            'updated_at' => date("Y-m-d H:i:s")
-        ]);
-
-        DB::table('locations')->insert([
-            'houseNumber' => '5',
-            'houseNumberAddition' => null,
-            'postalCode' => '5301BE',
-            'created_at' => date("Y-m-d H:i:s"),
-            'updated_at' => date("Y-m-d H:i:s")
-        ]);
-
-        DB::table('locations')->insert([
-            'houseNumber' => '1',
-            'houseNumberAddition' => null,
-            'postalCode' => '5342AJ',
-            'created_at' => date("Y-m-d H:i:s"),
-            'updated_at' => date("Y-m-d H:i:s")
-        ]);
-
-        DB::table('locations')->insert([
-            'houseNumber' => '10',
-            'houseNumberAddition' => null,
-            'postalCode' => '6525GA',
-            'created_at' => date("Y-m-d H:i:s"),
-            'updated_at' => date("Y-m-d H:i:s")
-        ]);
-
-        DB::table('locations')->insert([
-            'houseNumber' => '142',
-            'houseNumberAddition' => null,
-            'postalCode' => '5481AJ',
-            'created_at' => date("Y-m-d H:i:s"),
-            'updated_at' => date("Y-m-d H:i:s")
-        ]);
-
-        DB::table('locations')->insert([
-            'houseNumber' => '5',
-            'houseNumberAddition' => null,
-            'postalCode' => '5282HK',
-            'created_at' => date("Y-m-d H:i:s"),
-            'updated_at' => date("Y-m-d H:i:s")
-        ]);
-
-        DB::table('locations')->insert([
-            'houseNumber' => '9',
-            'houseNumberAddition' => null,
-            'postalCode' => '5268BZ',
-            'created_at' => date("Y-m-d H:i:s"),
-            'updated_at' => date("Y-m-d H:i:s")
-        ]);
-
-        DB::table('locations')->insert([
-            'houseNumber' => '1',
-            'houseNumberAddition' => null,
-            'postalCode' => '9711ME',
-            'created_at' => date("Y-m-d H:i:s"),
-            'updated_at' => date("Y-m-d H:i:s")
-        ]);
-
-        DB::table('locations')->insert([
-            'houseNumber' => '153',
-            'houseNumberAddition' => null,
-            'postalCode' => '2911RE',
-            'created_at' => date("Y-m-d H:i:s"),
-            'updated_at' => date("Y-m-d H:i:s")
-        ]);
-
-        DB::table('locations')->insert([
-            'houseNumber' => '2',
-            'houseNumberAddition' => null,
-            'postalCode' => '3439MX',
-            'created_at' => date("Y-m-d H:i:s"),
-            'updated_at' => date("Y-m-d H:i:s")
-        ]);
+        for($i = 0; $i < count($numbers); $i++){
+            DB::table('locations')->insert([
+                'houseNumber' => $numbers[$i],
+                'houseNumberAddition' => $additions[$i],
+                'postalCode' => $codes[$i],
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s")
+            ]);
+        }
     }
 }

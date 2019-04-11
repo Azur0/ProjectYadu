6941658fb4e93943fadddb8dcbf4818c06c0bd4e
azur0 <bimre@avans.nl>
2019-04-11 22:47:10 +0200

    + sidebar menu now works properly (desktop + mobile)

--- resources/views/layouts/admin/nav.blade.php
+++ resources/views/layouts/admin/nav.blade.php
@@ -2,7 +2,9 @@
 
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-        <div class="sidebar-brand-text mx-3"><img src="{{ URL::asset('/images/logoCircle.png') }}" width="50"></div>
+        <div class="sidebar-brand-icon">
+            <img src="{{ URL::asset('/images/logoCircle.png') }}" width="50">
+        </div>
     </a>
 
     <!-- Divider -->

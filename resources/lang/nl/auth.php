173fb54fe970915e51d0dac6a9a3e968553a35b0
SemicolonStruggles <38405582+SemicolonStruggles@users.noreply.github.com>
2019-04-19 18:43:52 +0200

    Translated Login [NL]

--- resources/lang/nl/auth.php
+++ resources/lang/nl/auth.php
@@ -13,7 +13,20 @@ return [
     |
     */
 
+    //    Do not edit failed and throttled keys, these are laravel defaults
     'failed' => 'Deze inloggegevens komen niet voor in onze database.',
     'throttle' => 'Te veel inlogpogingen. Probeer het opnieuw over :seconds seconden.',
+
+    ///////////////////////////////// Login ///////////////////////////////////
+
+    'login_login_title' => 'Inloggen',
+    'login_email' => 'E-Mailadres',
+    'login_password' => 'Wachtwoord',
+    'login_remember_me' => 'Ingelogd blijven',
+    'login_login' => 'Inloggen',
+    'login_forgot_password' => 'Wachtwoord vergeten?',
+    'login_register_new_account' => 'Nog geen account? Registreer <a href=":url">hier</a>.',
+
+    ///////////////////////////////// Register ////////////////////////////////
 
 ];

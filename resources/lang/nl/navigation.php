fb8b497fd406316d8fbbd776157d3b699ccf7849
SemicolonStruggles <38405582+SemicolonStruggles@users.noreply.github.com>
2019-04-19 14:25:07 +0200

    Created empty language files

---
+++ resources/lang/nl/navigation.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+/*
+|--------------------------------------------------------------------------
+| Navigation Language Lines
+|--------------------------------------------------------------------------
+*/
+
+///////////////////////////////// Navigation //////////////////////////////
+
+    'nav_home' => 'HOME',
+    'nav_events' => 'EVENEMENTEN',
+    'nav_about' => 'OVER ONS',
+    'nav_contact' => 'CONTACT',
+    'nav_profile' => 'PROFILE',
+    'nav_login' => 'INLOGGEN',
+    'nav_logout' => 'UITLOGGEN',
+
+///////////////////////////////// Footer //////////////////////////////////
+
+    'footer_about' => 'OVER YADU',
+    'footer_contact' => 'CONTACT',
+    'footer_tos' => 'VOORWAARDEN',
+    'footer_cookies' => 'COOKIES',
+    'footer_privacy' => 'PRIVACY',
+
+
+];

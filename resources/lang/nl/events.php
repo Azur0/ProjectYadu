1c38f4d4bd0e94edd754264a81d64d27fcc2afb5
SemicolonStruggles <38405582+SemicolonStruggles@users.noreply.github.com>
2019-04-19 16:21:31 +0200

    Translated events [NL]

--- resources/lang/nl/events.php
+++ resources/lang/nl/events.php
@@ -10,13 +10,62 @@ return [
 
 ///////////////////////////////// Global //////////////////////////////////
 
+    'cat1' => 'Uitje met gezinnen',
+    'cat2' => 'Eten',
+    'cat3' => 'Borrelen',
+    'cat4' => 'Koffie',
+    'cat5' => 'Wandelen',
+    'cat6' => 'Uitje in stad',
+    'cat7' => 'Museum',
+    'cat8' => 'Theater/film',
+    'cat9' => 'Evenementen',
+    'cat10' => 'Van alles wat',
+    'cat11' => 'Sporten',
+    'cat12' => 'Op wielen',
 
 ///////////////////////////////// Index ///////////////////////////////////
 
+    'index_create_event' => 'Organiseer een evenement',
+    'index_select_category' => 'Kies een categorie:',
+    'index_search_name' => 'Zoek op naam:',
+    'index_no_event_found' => 'Er kan geen event worden gevonden in uw buurt.',
+    'index_loading_error' => 'Er kan geen event worden geladen.',
 
 ///////////////////////////////// Create //////////////////////////////////
 
+    'create_step1' => 'Kies het type event',
+    'create_step2' => 'Kies een foto voor je event',
+    'create_step3' => 'Kies de (verzamel)locatie',
+    'create_step4' => 'Kies de datum en tijd',
+    'create_step5' => 'Beschrijf je uitje',
+    'create_step6' => 'Beschrijf je uitje',
+
+    'create_submit' => 'Verzenden',
+    'create_title' => 'Titel',
+    'create_description' => 'Beschrijving',
+
+
+    'create_characters_remaining' => 'tekens over',
+
+    'create_select_type_first' => 'Kies eerst het type event',
+    'create_amount_of_participants' => 'mensen kunnen mee (incl. jezelf)',
+
+    'create_error_select_photo' => 'Kies een foto',
+    'create_error_select_type' => 'Kies een type',
+    'create_error_location_required' => 'Het locatie-veld is verplicht.',
+    'create_error_invalid_datetime' => 'Deze datum/tijd is ongeldig.',
+    'create_error_title_required' => 'Het titel-veld is verplicht.',
+    'create_error_description_required' => 'Het omschrijving-veld is verplicht.',
+    'create_error_max_participants_required' => 'Het max aantal mensen-veld is verplicht.',
 
 ///////////////////////////////// Show ////////////////////////////////////
 
+    'show_location' => 'Locatie',
+    'show_date' => 'Datum en tijd',
+    'show_description' => 'Beschijving',
+    'show_initiator' => 'Initiatiefnemer',
+    'show_attendees' => 'Wie gaan er mee?',
+    'show_join' => 'Aanmelden',
+    'show_leave' => 'Afmelden',
+    'show_number_of_attendees' => ':amount van de :max deelnemers',
 ];

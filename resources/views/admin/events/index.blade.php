a6aed79eb8567c40c5990da73b99e31a19458400
Jeroen3529 <Jeroen3529@gmail.com>
2019-04-26 17:46:33 +0200

    hier dogen voor jouw

--- resources/views/admin/events/index.blade.php
+++ resources/views/admin/events/index.blade.php
@@ -2,23 +2,29 @@
 
 @section('content')
 
-	 <div>
-        <div class="search">
-            <label for="filterByTag">{{__('events.index_select_category')}}</label>
-            <input oninput="fetch_events()" list="tags" id="filterByTag" name="filterByTag"/>
-            <datalist id="tags">
-                @foreach ($tags as $tag)
-                    <option value="{{__('events.cat'.$tag->id)}}">
-                @endforeach
-            </datalist>
-            <label for="filterByName">{{__('events.index_search_name')}}</label>
-            <input oninput="fetch_events()" list="names" id="filterByName" name="filterByName" autocomplete="off"/>
-        </div>
-    </div>
-
 	<div class="card">
 		<div class="card-header">
-			<a href="/account/participating"><i class="fas fa-calendar-alt"></i> {{__('home.participating_title')}}</a>
+			<i class="fas fa-calendar-alt"></i> {{__('event.event')}} 
+			<span>
+				<a href="/events/create" class="btn btn-yadu-orange w-100"><i class="fas fa-user-friends"></i>&nbsp;{{__('events.index_create_event')}}
+				</a>
+			</span>
+			<div>
+				<div class="search">
+					<label for="filterByTag">{{__('events.index_select_category')}}</label>
+					<input oninput="fetch_events()" list="tags" id="filterByTag" name="filterByTag"/>
+					<datalist id="tags">
+						@foreach ($tags as $tag)
+							<option value="{{__('events.cat'.$tag->id)}}">
+						@endforeach
+					</datalist>
+					<label for="filterByName">{{__('events.index_search_name')}}</label>
+					<input oninput="fetch_events()" list="names" id="filterByName" name="filterByName" autocomplete="off"/>
+				</div>
+			</div>
+			<div class="col-12">
+			   
+			</div>
 		</div>
 		<div class="card-body">
 			<table class="table table-hover">
@@ -26,22 +32,20 @@
 				<tr>
 					<th scope="col"></th>
 					<th scope="col">ID</th>
-					<th scope="col">{{__('event.show_location')}}</th>
-					<th scope="col">{{__('home.participating_table_colname_owner')}}</th>
-					<th scope="col">{{__('home.participating_table_colname_date')}}</th>
-					<th scope="col">{{__('home.participating_table_colname_location')}}</th>
-					<th scope="col">{{__('home.participating_table_colname_max_participants')}}</th>
-                    <th scope="col">{{__('home.participating_table_colname_participants')}}</th>
+					<th scope="col">{{__('events.show_location')}}</th>
+					<th scope="col">{{__('events.show_initiator')}}</th>
+					<th scope="col">{{__('events.show_location')}}</th>
+					<th scope="col">{{__('events.show_location')}}</th>
+					<th scope="col">{{__('events.show_location')}}</th>
+					<th scope="col">{{__('events.show_location')}}</th>
 				</tr>
 				</thead>
-				<tbody >
-				@foreach($events as $event)
+				<tbody id="eventsToDisplay">
+					@foreach($events as $event)
 					<tr>
 						<td>
-							@if(  $event->isHighlighted == 0)
-								ster
-							@else
-								geenster
+							@if(  $event->isHighlighted == 1)
+								<i class="fas fa-star"></i>
 							@endif
 						</td>
 						<td>{{ $event->id}}</td>
@@ -49,8 +53,8 @@
 						<td>{{ $event->owner->firstName }} {{ $event->owner->middleName }} {{ $event->owner->lastName }}</td>
 						<td>{{ $event->date }}</td>
 						<td>{{ $event->location->postalcode }} {{ $event->city }}</td>
-                        <td>{{$event->numberOfPeople}}</td>
-                        <td>{{$event->participants->count()}}</td>
+						<td>{{$event->numberOfPeople}}</td>
+						<td>{{$event->participants->count()}}</td>
 					</tr>
 				@endforeach
 				</tbody>
@@ -58,69 +62,56 @@
 		</div>
 	</div>
 
-   
+	
 
-    <div class="row">
-        <div class="col-12">
-            <a href="/events/create" class="btn btn-yadu-orange w-100"><i
-                        class="fas fa-user-friends"></i>&nbsp;{{__('events.index_create_event')}}</a>
-        </div>
-    </div>
+	<script type="text/javascript">
+		//AJAX request
+		function fetch_events() {
+			$('#eventsToDisplay').html("<img class='loadingSpinner' src='images/Spinner-1s-200px.gif'>");
+			//var distance;
+			//distance = $("#rangeValue").val();
+			var inputTag = $(filterByTag).val();
+			var inputName = $(filterByName).val();
+			$.ajax({
+				url: "{{ route('events_controller.actionDistanceFilter')}}",
+				method: 'POST',
+				data: {
+					distance: 25,
+					inputTag: inputTag,
+					inputName: inputName,
+					_token: '{{ csrf_token() }}'
+				},
+				dataType: 'json',
+				success: function (data) {
+					console.log(data);
+					if (data == "") {
+						$('#eventsToDisplay').html("<tr>{{__('events.index_no_event_found')}}</tr>");
+					} else {
+						$('#eventsToDisplay').html("");
+						data.forEach(function (element)
+						{
+							`<tr>
+								<td></td>
+								<td> element['owner'] </td>
+								<td> element['owner'] </td>
+								<td> element['owner'] </td>
+								<td> element['owner'] </td>
+								<td> element['owner'] </td>
+								<td> element['owner'] </td>
+								<td> element['owner'] </td>
+							</tr>`
+						});
+					}
+				},
+				error: function (jqXHR, textStatus, errorThrown) {
+					$('#eventsToDisplay').html(
+						//TODO TRANSLATION
+						"<div style='text-align:center; width:100%; padding-top:50px;'><h1>{{__('events.index_loading_error')}}</h1><div>"
+					);
+				}
+			})
 
-    <div class="event_overview row" id="eventsToDisplay">
-        <img class='loadingSpinner' src='images/Spinner-1s-200px.gif'>
-    </div>
-
-    <script type="text/javascript">
-        //AJAX request
-        function fetch_events() {
-            $('#eventsToDisplay').html("<img class='loadingSpinner' src='images/Spinner-1s-200px.gif'>");
-            //var distance;
-            //distance = $("#rangeValue").val();
-            var inputTag = $(filterByTag).val();
-            var inputName = $(filterByName).val();
-            $.ajax({
-                url: "{{ route('events_controller.actionDistanceFilter')}}",
-                method: 'POST',
-                data: {
-                    //distance: distance,
-                    inputTag: inputTag,
-                    inputName: inputName,
-                    _token: '{{ csrf_token() }}'
-                },
-                dataType: 'json',
-                success: function (data) {
-                    console.log(data);
-                    if (data == "") {
-                        $('#eventsToDisplay').html(
-                            //TODO remove inline style
-                            //TODO TRANSLATION
-                            "<div style='text-align:center; width:100%; padding-top:50px;'><h1>{{__('events.index_no_event_found')}}</h1><div>"
-                        );
-                    } else {
-                        $('#eventsToDisplay').html("");
-                        data.forEach(function (element) {
-                            $('#eventsToDisplay').html($("#eventsToDisplay").html() +
-                                "<div class='col-md-6 col-lg-4 event'><a href='/events/" + element[
-                                    'id'] +
-                                "'><div class='card mb-4 box-shadow'> <img class = 'card-img-top' src ='data:image/jpeg;base64, " +
-                                element['picture'] +
-                                "' alt = 'Card image cap'><div class = 'event_info' > <h3> " +
-                                element['eventName'] + "</h3><p>" + element['date'] +
-                                "<br>" + element['loc'] +
-                                "</p></div></div></a></div>");
-                        });
-                    }
-                },
-                error: function (jqXHR, textStatus, errorThrown) {
-                    $('#eventsToDisplay').html(
-                        //TODO TRANSLATION
-                        "<div style='text-align:center; width:100%; padding-top:50px;'><h1>{{__('events.index_loading_error')}}</h1><div>"
-                    );
-                }
-            })
-
-        }
-    </script>
+		}
+	</script>
 
 @endsection

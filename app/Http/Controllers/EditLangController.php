b162f45f8f1bd9b2d34ef357697054e43e4bee34
Rubenwest <rswester@avans.nl>
2019-04-28 16:27:09 +0200

    Als het goed is werkt hij atm met arrays, moet alleen nog ff beter testen, wil nu eerst dev in deze branch hebben

--- app/Http/Controllers/EditLangController.php
+++ app/Http/Controllers/EditLangController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\Facades\App;
-
+use Hamcrest\Type\IsArray;
 
 class EditLangController extends Controller
 {
@@ -15,30 +15,77 @@ class EditLangController extends Controller
         App::setLocale($lang);
         $x = __($page);
         App::setLocale($currentLocale);
+        // dd($x);
         return view("admin.editText", compact("x","page", "lang"));
     }
 
     public function saveFile(Request $request){
         $request->validate([
             'lang' => 'required',
-            'file' => 'required',
+            'lang_file' => 'required',
         ]);
+        $currentLocale = app()->getLocale();
+        App::setLocale($request->lang);
+        $langArrayFromFile = __($request->lang_file);
+        App::setLocale($currentLocale);
+        // dd($langArrayFromFile);
 
-        $fileName = base_path()."/resources/lang/".$request->lang."/".$request->file.".php";
+        $fileName = base_path()."/resources/lang/".$request->lang."/".$request->lang_file.".php";
         $fileBegin = "<?php return [";
-        $fileMiddle = "";
+       
 
         $values = $request->all();
-        unset($values["file"]);
-        unset($values["_token"]);
+        // dd($values);
         unset($values["lang"]);
-
-        foreach ($values as $key => $value){
-            $fileMiddle .=" '$key' => '$value', ";
-        }
+        unset($values["_token"]);
+        unset($values["lang_file"]);
+        
+        $fileMiddle = $this->makeMiddle($values, $langArrayFromFile, "", "");
+        
+        // dd($fileMiddle);
 
         $fileEnd = "];";
         file_put_contents($fileName, $fileBegin.$fileMiddle.$fileEnd);
         return redirect('/admin');
+    }
+    function makeMiddle($input, $baseFile, $fileMiddle, $baseKey){
+        $out = new \Symfony\Component\Console\Output\ConsoleOutput();
+        
+        foreach ($baseFile as $key => $value){
+            
+            // if($baseKey!=""){
+            //     dd($baseFile);
+            // }
+            
+            if(is_array($value)){
+             
+                // if($baseKey!=""){
+                //     dd($baseFile);
+                // }
+                $fileMiddle .= "'$key' => [";
+               
+                
+                $fileMiddle = $this->makeMiddle($input, $value, $fileMiddle, $key);
+                
+                $fileMiddle .= " ], "; 
+            }else{
+                // $out->writeln("--> key ".$key);
+                // $out->writeln("--> value ".$value);
+                if($baseKey != ""){
+                    $valueFromInput = $input["$baseKey;$key"];
+                }
+                else{
+                    $valueFromInput = $input["$key"];    
+                }
+                
+                $fileMiddle .=" '$key' => '$valueFromInput', ";
+                // dd($fileMiddle);
+            }
+        }
+        // if($baseKey==""){
+        //     dd($fileMiddle);
+        // }
+        //  dd($fileMiddle);
+        return $fileMiddle;
     }
 }
--- resources/views/admin/editText.blade.php
+++ resources/views/admin/editText.blade.php
@@ -4,43 +4,56 @@
 
 <h1>Edit pagina</h1>
 <form action="/admin" method="post">
-@csrf
-<input type="hidden" name="file" value="{{$page}}">
-<input type="hidden" name="lang" value="{{$lang}}">
+    @csrf
+    <input type="hidden" name="lang_file" value="{{$page}}">
+    <input type="hidden" name="lang" value="{{$lang}}">
 
-<?php
+    <?php
+$addedString = "";
+
 function isObjectArray($key, $item){
-    printTitle($key);
+    $primaryKey = $key;
     foreach($item as $key => $singleItem){
         if (is_array($singleItem)){
+            printTitle($key);
+            global $addedString;
+            $addedString .= "---";
             isObjectArray($key, $singleItem);
         }
         else {
-            printInputFields($key, $singleItem);
+            printInputFields($key, $singleItem, $primaryKey);
         }
-    }
+    } 
 }
 
 function printTitle($key){
-    echo "<p><strong>$key</strong></p>";
+    global $addedString;
+    echo "<p><strong>$addedString $key</strong></p>";
 }
 
-function printInputFields($key, $item){
-    echo "<p> - $key - <input type='text' name='$key' id='EditInput' value='$item'></p>";
+function printInputFields($key, $item, $primaryKey){
+    global $addedString;
+    echo "<p> $addedString - $key <input type='text' name='$primaryKey;$key' id='EditInput' value='$item'></p>";
 }
 
 ?>
 
-@foreach ($x as $key => $item)
+    @foreach ($x as $key => $item)
     @if (!is_array($item))
-        <p>{{$key}} - <input type="text" name="{{$key}}" id="EditInput" value="{{$item}}"></p>
+    <?php global $addedString; $addedString = "";?>
+    <p>{{$key}} - <input type="text" name="{{$key}}" id="EditInput" value="{{$item}}"></p>
     @else
-        <?php isObjectArray($key, $item) ?>
+    <?php 
+        global $addedString; 
+        $addedString = ""; 
+        printTitle($key); 
+        $addedString = "---"; 
+        isObjectArray($key, $item); ?>
     @endif
-    
-@endforeach
 
-<input type="submit" value="submit">
+    @endforeach
+
+    <input type="submit" value="submit">
 </form>
 
 @endsection

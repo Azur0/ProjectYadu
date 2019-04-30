420187b0beb324df2a381c2f43bcb991c292c628
SemicolonStruggles <38405582+SemicolonStruggles@users.noreply.github.com>
2019-04-30 15:13:05 +0200

    Switched to middleware so it can be applied to all routes

--- routes/web.php
+++ routes/web.php
@@ -47,5 +47,5 @@ Auth::routes();
 
 Route::resource('events', 'EventsController');
 
-Route::get('admin','Management\AdminController@index');
+Route::get('admin', function () { return view('admin.index');})->middleware('auth', 'isAdmin');
 

cd38700036aff495a38eba9788edfc1e516175ec
default-kaas <dogen1996@live.nl>
2019-05-01 15:46:07 +0200

    Fix nav problem

--- resources/views/layouts/admin/nav.blade.php
+++ resources/views/layouts/admin/nav.blade.php
@@ -74,26 +74,7 @@
             </div>
         </div>
     </li>
-
-	<!-- Heading-->
-	<div class="sidebar-heading">
-		Content
-	</div>
-
-	<!--Nav Item - Pages--> 
-	<li class="nav-item">
-		<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
-			<i class="fas fa-fw fa-cog"></i>
-			<span>Pagina's</span>
-		</a>
-		<div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-			<div class="bg-white py-2 collapse-inner rounded">
-				<h6 class="collapse-header">Beheren:</h6>
-				<a class="collapse-item" href="{{ url('admin')  }}">Placeholder link</a>
-			</div>
-		</div>
-	</li>
-
+	
 	<!-- Divider -->
 	<hr class="sidebar-divider">
 
@@ -116,35 +97,6 @@
             </div>
         </div>
     </li>
-
-    <!-- Nav Item - Events -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseEvents" aria-expanded="true"
-            aria-controls="collapseEvents">
-            <i class="fas fa-fw fa-users"></i>
-            <span>Evenementen</span>
-        </a>
-        <div id="collapseEvents" class="collapse" aria-labelledby="headingEvents" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Beheren:</h6>
-                <a class="collapse-item" href="{{ url('admin')  }}">Placeholder link</a>
-            </div>
-        </div>
-    </li>
-
-	<!-- Nav Item - Users-->
-	<li class="nav-item">
-		<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUsers" aria-expanded="true" aria-controls="collapseUsers">
-			<i class="fas fa-fw fa-users"></i>
-			<span>Gebruikers</span>
-		</a>
-		<div id="collapseUsers" class="collapse" aria-labelledby="headingUsers" data-parent="#accordionSidebar">
-			<div class="bg-white py-2 collapse-inner rounded">
-				<h6 class="collapse-header">Beheren:</h6>
-				<a class="collapse-item" href="{{ url('admin')  }}">Users: Placeholder link</a>
-			</div>
-		</div>
-	</li>
 
 	<!-- Nav Item - Events -->
 	<li class="nav-item">

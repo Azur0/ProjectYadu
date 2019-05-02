c5ccf949a96fc06e64114fff7575b2654a50596f
SemicolonStruggles <38405582+SemicolonStruggles@users.noreply.github.com>
2019-05-02 12:58:28 +0200

    Removed unnecessary variable

--- app/Traits/Encryptable.php
+++ app/Traits/Encryptable.php
@@ -10,7 +10,7 @@ trait Encryptable
         $value = parent::getAttribute($key);
 
         if (in_array($key, $this->encryptable)) {
-            return $value = Crypt::decrypt($value);
+            return Crypt::decrypt($value);
         }
         return $value;
     }

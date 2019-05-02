ab9b78f61c73b60944d4222abe6148dc50fc825e
default-kaas <dogen1996@live.nl>
2019-05-02 19:54:58 +0200

    Remove button that does not work

--- resources/views/layouts/admin/nav.blade.php
+++ resources/views/layouts/admin/nav.blade.php
@@ -113,12 +113,12 @@
 		</div>
 	</li>
 
-	<!-- Divider -->
+	<!-- Divider
 	<hr class="sidebar-divider d-none d-md-block">
 
-	<!-- Sidebar Toggler (Sidebar) -->
+	 Sidebar Toggler (Sidebar)
 	<div class="text-center d-none d-md-inline">
 		<button class="rounded-circle border-0" id="sidebarToggle"></button>
-	</div>
+	</div> -->
 
 </ul>

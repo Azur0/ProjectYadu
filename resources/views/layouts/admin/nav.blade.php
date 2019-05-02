99860431e98148cfb32a43a02c3f78df490c3490
default-kaas <dogen1996@live.nl>
2019-05-02 20:14:10 +0200

    Add pages nav name translation

--- resources/views/layouts/admin/nav.blade.php
+++ resources/views/layouts/admin/nav.blade.php
@@ -33,7 +33,7 @@
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true"
             aria-controls="collapsePages">
             <i class="fas fa-fw fa-cog"></i>
-            <span>Pagina's</span>
+            <span>{{__('navigation.nav_pages')}}</span>
         </a>
         <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">

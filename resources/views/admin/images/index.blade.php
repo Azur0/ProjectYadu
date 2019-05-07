8c221450ed7935bb1ed3046ef9ea4924b2904123
Merijn Monfils <merijn.monfils@gmail.com>
2019-05-07 23:55:29 +0200

    placeholder popup

--- resources/views/admin/images/index.blade.php
+++ resources/views/admin/images/index.blade.php
@@ -9,7 +9,7 @@
             <div class="box">
                 @foreach ($tags as $Tag)
                     <input type="radio" id="{{$Tag->tag}}" name="tag" value="{{$Tag->id}}"
-                            onclick="check({{$Tag->id }})">
+                          onclick="popup('popUpDiv')"  onclick="check({{$Tag->id }})">
                     <label for="{{$Tag->tag}}" class="category">
                         <?php echo '<img class="default" src="data:image/jpeg;base64,' . base64_encode($Tag->imageDefault) . '"/>'; ?>
                         <?php echo '<img class="selected" src="data:image/jpeg;base64,' . base64_encode($Tag->imageSelected) . '"/>'; ?>
@@ -26,4 +26,14 @@
             <div class="error">{{__('events.error_select_photo')}}</div>
         @endif
 </div>
+
+
+{{-- popup --}}
+<div id="blanket" style="display:none;"></div>
+<div id="popUpDiv" style="display:none;">
+    <a href="#" onclick="popup('popUpDiv')" >Click to Close CSS Pop Up</a>
+    <p>test</p>
+</div>
+
+<script src="..\js\popup.js"></script>
 @endsection

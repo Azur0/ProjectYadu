7090f912460df75a3e95ef867a53945766f74603
Martijn Blom <mblom5@avans.nl>
2019-05-13 12:06:59 +0200

    no message

--- resources/views/layouts/admin/nav.blade.php
+++ resources/views/layouts/admin/nav.blade.php
@@ -115,7 +115,7 @@
 
 	<!-- Nav Item - Swear words -->
 	<li class="nav-item {{ request()->is('admin/swearWords') ? 'active' : '' }}">
-		<a class="nav-link collapsed" href="{{ url('admin/accounts')  }}">
+		<a class="nav-link collapsed" href="{{ url('admin/swearWords')  }}">
 			<i class="fas fa-fw fa-users"></i>
 			<span>{{__('navigation.nav_swearWords')}}</span>
 		</a>

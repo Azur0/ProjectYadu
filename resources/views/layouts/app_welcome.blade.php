032d57f6dcd84226ad94ae6043a2eab37fe1fe89
SemicolonStruggles <38405582+SemicolonStruggles@users.noreply.github.com>
2019-05-21 23:14:31 +0200

    Added Google Analytics script

--- resources/views/layouts/app_welcome.blade.php
+++ resources/views/layouts/app_welcome.blade.php
@@ -1,6 +1,21 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}" dir="ltr">
     <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <!-- TODO: this page needs to extend app.blade.php so this scrip is automatically imported -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-140653687-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+
+            gtag('js', new Date());
+
+            gtag('config', 'UA-140653687-1');
+        </script>
+
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">

a0ca51330bc7b7dbbbc1d02d2fa487a2f0c0c4f2
default-kaas <dogen1996@live.nl>
2019-05-24 14:41:23 +0200

    Still no fix for image problem mail

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,5 +1,12 @@
 <tr class="header" style="background-color: #F39200;">
     <td class="header" style="background-color: #F39200;">
-        <div style="background-color:#F39200"><img style="margin-right: 45%" src={{ asset('images/logoCircle.png')}} width="57" height="57"  alt="logo"></div>
+        <div style="background-color:#F39200">
+            <img style="margin-right: 45%"
+                 src="{{asset('/images/logoCircle.png')}}"
+                 alt="{{config('app.name')}}"
+                 width="57"
+                 height="57"
+                 >
+        </div>
     </td>
 </tr>

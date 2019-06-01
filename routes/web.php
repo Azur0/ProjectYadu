dbf29a2b6d19e2ef270e37bc3189cb0f806178a4
default-kaas <dogen1996@live.nl>
2019-06-01 16:24:00 +0200

    Moved controllers

--- routes/web.php
+++ routes/web.php
@@ -28,12 +28,6 @@ Route::get('/contact', function () { $socialmedia = socialmedia::all(); return v
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/edit/{lang}/{page}', 'EditLangController@index')->middleware('auth', 'isAdmin');
-Route::post('admin', 'EditLangController@saveFile')->middleware('auth', 'isAdmin');
-
-Route::get('admin/links', 'EditLinksController@index')->middleware('auth', 'isAdmin');
-Route::post('admin/link', 'EditLinksController@saveLink')->middleware('auth', 'isAdmin');
-Route::post('admin/email', 'EditLinksController@saveEmail')->middleware('auth', 'isAdmin');
 
 Route::get('/account/myevents', 'HomeController@myEvents')->middleware('auth');
 Route::get('/account/participating', 'HomeController@participating')->middleware('auth');
@@ -51,16 +45,19 @@ Route::post('/events/actionDistanceFilter', 'EventsController@actionDistanceFilt
 Auth::routes(['verify' => true]);
 
 //Profile
+//Why edit an not update
 Route::get('/profile/edit', 'AccountController@edit')->middleware('auth');
+//Extra
+Route::patch('/profile/updateAccountSettings/{id}', 'AccountController@updateSettings')->middleware('auth');
+Route::post('/profile/updatePrivacySettings', 'AccountController@updatePrivacySettings')->middleware('auth');
+Route::post('/profile/updateProfile', 'AccountController@updateProfile')->middleware('auth');
+
 Route::get('/profile/{id}/follow', 'AccountController@follow')->middleware('auth');
 Route::get('/profile/{id}/accept', 'AccountController@accept')->middleware('auth');
 Route::get('/profile/{id}/decline', 'AccountController@decline')->middleware('auth');
 Route::get('/profile/{id}/unfollow', 'AccountController@unfollow')->middleware('auth');
-Route::post('/profile/updateProfile', 'AccountController@updateProfile')->middleware('auth');
-Route::post('/profile/updatePrivacySettings', 'AccountController@updatePrivacySettings')->middleware('auth');
 Route::post('/profile/changePassword', 'AccountController@changePassword')->middleware('auth');
 Route::post('/profile/deleteAccount', 'AccountController@deleteAccount')->middleware('auth');
-Route::patch('/profile/updateAccountSettings/{id}', 'AccountController@updateSettings')->middleware('auth');
 Route::post('/profile/blockUser', 'AccountController@blockAccount')->middleware('auth');
 Route::post('/profile/unblockUser/', 'AccountController@unblockAccount')->middleware('auth');
 
@@ -72,31 +69,46 @@ Route::get('admin', function () { return view('admin.index');})->middleware('aut
 
 Route::post('/language', 'LanguageController@setLanguage');
 
-// Why did you not use resource for index/show/delete/update?
-Route::get('admin/accounts', 'Management\AccountsController@index')->middleware('auth', 'isAdmin');
-Route::get('admin/accounts/{id}', 'Management\AccountsController@show')->middleware('auth', 'isAdmin');
-Route::get('admin/accounts/{id}/activate', 'Management\AccountsController@activate')->middleware('auth', 'isAdmin');
-Route::get('admin/accounts/{id}/delete', 'Management\AccountsController@destroy')->middleware('auth', 'isAdmin');
-Route::post('admin/accounts/{id}/update', 'Management\AccountsController@update')->middleware('auth', 'isAdmin');
-Route::get('admin/accounts/{id}/avatarreset', 'Management\AccountsController@resetavatar')->middleware('auth', 'isAdmin');
+// Admin accounts
+//CRUD
+Route::get('admin/accounts', 'Admin\AccountsController@index')->middleware('auth', 'isAdmin');
+Route::get('admin/accounts/{id}', 'Admin\AccountsController@show')->middleware('auth', 'isAdmin');
+Route::get('admin/accounts/{id}/delete', 'Admin\AccountsController@destroy')->middleware('auth', 'isAdmin');
+Route::post('admin/accounts/{id}/update', 'Admin\AccountsController@update')->middleware('auth', 'isAdmin');
+//Extra
+Route::get('admin/accounts/{id}/activate', 'Admin\AccountsController@activate')->middleware('auth', 'isAdmin');
+Route::get('admin/accounts/{id}/avatarreset', 'Admin\AccountsController@resetavatar')->middleware('auth', 'isAdmin');
+Route::post('admin/accounts/action', 'Admin\AccountsController@action')->name('admin_accounts_controller.action');
 
-Route::post('admin/accounts/action', 'Management\AccountsController@action')->name('admin_accounts_controller.action');
+//Admin events
+Route::resource('admin/events','Admin\EventsController');
+Route::post('/admin/events/actionDistanceFilter', 'Admin\EventsController@actionDistanceFilter')->name('admin_events_controller.actionDistanceFilter');
 
-Route::resource('admin/events','Management\EventsController');
-Route::post('/admin/events/actionDistanceFilter', 'Management\EventsController@actionDistanceFilter')->name('admin_events_controller.actionDistanceFilter');
+//Admin prohibitedWords
+Route::resource('admin/swearWords','Admin\ProhibitedWordsController');
+//CRUD -> why /\ this if use this \/
+Route::get('admin/prohibitedWords', 'Admin\ProhibitedWordsController@index')->middleware('auth', 'isAdmin');
+Route::post('admin/prohibitedWords/delete', 'Admin\ProhibitedWordsController@destroy')->middleware('auth', 'isAdmin');
+Route::post('admin/prohibitedWords/update', 'Admin\ProhibitedWordsController@update')->middleware('auth', 'isAdmin');
+Route::post('admin/prohibitedWords/create', 'Admin\ProhibitedWordsController@create')->middleware('auth', 'isAdmin');
 
-Route::resource('admin/swearWords','Management\ProhibitedWordsController');
-Route::get('admin/prohibitedWords', 'Management\ProhibitedWordsController@index')->middleware('auth', 'isAdmin');
-Route::post('admin/prohibitedWords/delete', 'Management\ProhibitedWordsController@destroy')->middleware('auth', 'isAdmin');
-Route::post('admin/prohibitedWords/update', 'Management\ProhibitedWordsController@update')->middleware('auth', 'isAdmin');
-Route::post('admin/prohibitedWords/create', 'Management\ProhibitedWordsController@create')->middleware('auth', 'isAdmin');
-
+//Admin logger
 Route::post('/logger/eventshared', 'LogController@LogEventShared')->name('LogEventShared');
 
-Route::post('/charts/totaleventscreated', 'ChartController@GetTotalEventsCreated')->name('admin_charts_events')->middleware('auth', 'isAdmin');
-Route::post('/charts/shares', 'ChartController@GetShares')->name('admin_charts_shares')->middleware('auth', 'isAdmin');
-Route::post('/charts/activeeventlocations', 'ChartController@GetActiveEventLocations')->name('admin_charts_locations')->middleware('auth', 'isAdmin');
-Route::post('/charts/categories', 'ChartController@GetCategories')->name('admin_charts_categories')->middleware('auth', 'isAdmin');
-Route::post('/charts/chatmessages', 'ChartController@GetChatmessages')->name('admin_charts_chatmessages')->middleware('auth', 'isAdmin');
-Route::post('/charts/accountscreated', 'ChartController@GetAccountsCreated')->name('admin_charts_accounts_created')->middleware('auth', 'isAdmin');
-Route::post('/charts/updatedatesting', 'ChartController@UpdateDateString')->name('admin_charts_update_date_string')->middleware('auth', 'isAdmin');
+//Admin charts
+Route::post('/charts/totaleventscreated', 'Admin\ChartController@GetTotalEventsCreated')->name('admin_charts_events')->middleware('auth', 'isAdmin');
+Route::post('/charts/shares', 'Admin\ChartController@GetShares')->name('admin_charts_shares')->middleware('auth', 'isAdmin');
+Route::post('/charts/activeeventlocations', 'Admin\ChartController@GetActiveEventLocations')->name('admin_charts_locations')->middleware('auth', 'isAdmin');
+Route::post('/charts/categories', 'Admin\ChartController@GetCategories')->name('admin_charts_categories')->middleware('auth', 'isAdmin');
+Route::post('/charts/chatmessages', 'Admin\ChartController@GetChatmessages')->name('admin_charts_chatmessages')->middleware('auth', 'isAdmin');
+Route::post('/charts/accountscreated', 'Admin\ChartController@GetAccountsCreated')->name('admin_charts_accounts_created')->middleware('auth', 'isAdmin');
+Route::post('/charts/updatedatesting', 'Admin\ChartController@UpdateDateString')->name('admin_charts_update_date_string')->middleware('auth', 'isAdmin');
+
+//Admin language
+Route::get('/edit/{lang}/{page}', 'Admin\EditLangController@index')->middleware('auth', 'isAdmin');
+Route::post('admin', 'Admin\EditLangController@saveFile')->middleware('auth', 'isAdmin');
+
+//Admin Links
+Route::get('admin/links', 'Admin\EditLinksController@index')->middleware('auth', 'isAdmin');
+Route::post('admin/link', 'Admin\EditLinksController@saveLink')->middleware('auth', 'isAdmin');
+Route::post('admin/email', 'Admin\EditLinksController@saveEmail')->middleware('auth', 'isAdmin');

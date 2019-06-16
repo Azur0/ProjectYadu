ec1738580df1109a5ae4ddf9f3f0b15e73c736c1
Rubenwest <rswester@avans.nl>
2019-06-16 19:57:39 +0200

    Bijna af

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,17 +14,17 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
 
         $this->call(AccountRolesTableSeeder::class);
-        $this->call(LocationsTableSeeder::class);
+        // $this->call(LocationsTableSeeder::class);
         $this->call(GendersTableSeeder::class);
-        $this->call(AccountsTableSeeder::class);
+        // $this->call(AccountsTableSeeder::class);
         $this->call(EventTagsTableSeeder::class);
         $this->call(EventPicturesTableSeeder::class);
-        $this->call(EventsTableSeeder::class);
+        // $this->call(EventsTableSeeder::class);
         $this->call(ProhibitedWordsTableSeeder::class);
-        $this->call(AccountSettingsTableSeeder::class);
+        // $this->call(AccountSettingsTableSeeder::class);
         $this->call(SocialmediaTableSeeder::class);
-        $this->call(SocialMediaPlatformsTableSeeder::class);
-        $this->call(AccountHasFollowersTableSeeder::class);
+        // $this->call(SocialMediaPlatformsTableSeeder::class);
+        // $this->call(AccountHasFollowersTableSeeder::class);
 
     }
 }

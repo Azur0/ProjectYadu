57e8806fcffcbb601a47877e9ec6d83cb491a99e
SemicolonStruggles <38405582+SemicolonStruggles@users.noreply.github.com>
2019-06-18 01:08:25 +0200

    update testimonial migration

--- app/Testemonial.php
+++ app/Testemonial.php
@@ -6,10 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Testemonial extends Model
 {
-    protected $fillable = ['account_id', 'name', 'experience', 'accepted'];
-
-    public function account()
-    {
-        return $this->belongsTo('App\Account', 'account_id', 'id');
-    }
+    protected $fillable = ['name', 'experience'];
 }

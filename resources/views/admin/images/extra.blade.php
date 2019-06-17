570b0d0ca068727dab2ee569fde8e2ef85086ea5
Merijn Monfils <merijn.monfils@gmail.com>
2019-06-17 23:08:30 +0200

    float right removed

--- resources/views/admin/images/extra.blade.php
+++ resources/views/admin/images/extra.blade.php
@@ -5,7 +5,7 @@
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
     <div>
         @if(count($errors) > 0)
-            <div class="alert alert-danger">
+            <div class="alert">
                 <ul>
                     @foreach($errors->all() as $error)
                         <li>{{ $error }}</li>

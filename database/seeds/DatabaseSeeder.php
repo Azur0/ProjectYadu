c837cfc4ea945aa158a531e56cd823b459e56ffa
SemicolonStruggles <38405582+SemicolonStruggles@users.noreply.github.com>
2019-06-17 20:25:04 +0200

    Modified seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,20 +11,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-
         $this->call(AccountRolesTableSeeder::class);
-        // $this->call(LocationsTableSeeder::class);
         $this->call(GendersTableSeeder::class);
-        // $this->call(AccountsTableSeeder::class);
+        $this->call(AccountsTableSeeder::class);
         $this->call(EventTagsTableSeeder::class);
         $this->call(EventPicturesTableSeeder::class);
-        // $this->call(EventsTableSeeder::class);
         $this->call(ProhibitedWordsTableSeeder::class);
-        // $this->call(AccountSettingsTableSeeder::class);
+        $this->call(AccountSettingsTableSeeder::class);
         $this->call(SocialmediaTableSeeder::class);
-        // $this->call(SocialMediaPlatformsTableSeeder::class);
-        // $this->call(AccountHasFollowersTableSeeder::class);
-
+        $this->call(SocialMediaPlatformsTableSeeder::class);
     }
 }

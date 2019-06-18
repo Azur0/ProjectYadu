b4bd00ff50640f4c6a3f68963be172e7cf7662ad
Jeroen3529 <Jeroen3529@gmail.com>
2019-06-18 10:50:26 +0200

    error

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -4,7 +4,6 @@
 	<div id="http_error">
 		<h1>500 :(</h1>
 		<div>
-			<h2>{{ $exception->getMessage() }}</h2>
 			<p>{{__('httperror.500')}}</p>
 			<a href="{{ url()->previous() }}"><i class="fas fa-arrow-left"></i> {{__('httperror.back')}}</a>
 		</div>
